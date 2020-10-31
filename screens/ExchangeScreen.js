0e2b5b28b98bba34fd8d89cd05ae4b01c1992cf1
Kanav Mathur <kanavmathur30@gmail.com>
2020-10-31 13:17:53 +0530

    Add files via upload

---
+++ screens/ExchangeScreen.js
@@ -0,0 +1,121 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, TextInput, TouchableOpacity, Alert,
+  ScrollView, KeyboardAvoidingView } from 'react-native';
+import db from '../config';
+import firebase from 'firebase';
+
+
+export default class ExchangeScreen extends React.Component {
+  constructor(){
+    super()
+    this.state={
+      userName: firebase.auth().currentUser.email,
+      itemName: '',
+      itemDescription: '',
+    }
+  }
+
+  addItem=(itemName, itemDescription)=>{
+    var userName = this.state.userName
+    db.collection('Exchange').add({
+      "userName": userName,
+      "itemName": itemName,
+      "itemDescription": itemDescription
+    })
+    this.setState({
+      itemName: '',
+      itemDescription: ''
+    })
+    return Alert.alert(
+      'Item Ready To Exchange',
+      '',
+      [
+        {text: 'OK', onPress:()=>{
+          this.props.navigation.navigate('HomeScreen')
+        }}
+      ]
+    )
+  }
+
+
+  render(){
+    return (
+      <View>
+        <KeyboardAvoidingView style = {styles.keyboardStyle}>
+        <TextInput
+        style={styles.Textinput}
+        placeholder = {'Enter Item Name'}
+        onChangeText = {(text)=>{
+          this.setState({itemName: text})
+        }}
+        value = {this.state.itemName}
+        />
+        <TextInput 
+        style={styles.Textinput2}
+        multiline
+        numberOfLines = {10}
+        placeholder = {'Item Description'}
+        onChangeText = {(text)=>{
+          this.setState({itemDescription: text})
+        }}
+        value = {this.state.itemDescription}
+        />
+        <TouchableOpacity
+        style={{
+        backgroundColor: '#3282b8',
+        height: 40,
+        width: 80,
+        height: 60,
+        alignSelf: 'center'}}
+        onPress={()=>{
+        this.addItem(this.state.itemName, this.state.itemDescription)
+        }}
+        >
+          <Text
+          style={{alignSelf: 'center',
+          textAlign: 'center',
+          alignContent: 'center',
+          color: 'white',
+          fontSize: 20,
+          margin:3,
+          color: 'black',
+          fontWeight: 'bold'
+        }}
+          >
+           Add Item
+          </Text>
+        </TouchableOpacity>
+    </KeyboardAvoidingView>
+   
+      </View>
+    );
+    }
+  
+  }
+  
+  const styles = StyleSheet.create({
+    keyboardStyle: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    Textinput:{
+     height : 38,
+     fontSize: 28,
+     width: 300,
+     alignSelf: "center",
+     textAlign: "center",
+     margin: 10,
+     borderWidth: 2.5 
+   },
+   Textinput2:{
+    height : 38,
+    fontSize: 28,
+    width: 300,
+    height: 300,
+    alignSelf: "center",
+    textAlign: "center",
+    margin: 10,
+    borderWidth: 2.5 
+   }
+   });
